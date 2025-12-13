c0e0ebc61dc0fdf204db789cabadf110c05cf240
Josh Martin <joshua.martin1@edu.sait.ca>
2025-12-12 23:40:03 -0700

    Add dynamic export to CreateAccount page

--- app/CreateAccount/page.js
+++ app/CreateAccount/page.js
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
+
 import { useState, useEffect } from "react";
 import { useUserAuth } from "../_utils/auth-context";
 import { redirect } from "next/navigation";

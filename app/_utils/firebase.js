9f54bbbab5ce9f0875446a05461c939dba58c20f
Josh Martin <joshua.martin1@edu.sait.ca>
2025-12-13 00:55:23 -0700

    firebase config

--- app/_utils/firebase.js
+++ app/_utils/firebase.js
@@ -1,20 +1,14 @@
-import { initializeApp, getApp, getApps } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+ 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDFicwQ1fVJm1Oyul7t1Cctr2dnsdrKBb8",
+  authDomain: "cloudp3-test.firebaseapp.com",
+  projectId: "cloudp3-test",
+  storageBucket: "cloudp3-test.firebasestorage.app",
+  messagingSenderId: "1019546303900",
+  appId: "1:1019546303900:web:acba3eda1a46b79f69dc59",
 };
-
-const app =
-  typeof window !== "undefined"
-    ? getApps().length
-      ? getApp()
-      : initializeApp(firebaseConfig)
-    : null;
-
-export const auth = app ? getAuth(app) : null;
+ 
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
